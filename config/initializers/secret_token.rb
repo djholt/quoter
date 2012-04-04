c12a528c9302907bd3ed20cf8cb38ea61f627bea
D. J. Holt <djholt@cloakedhunter.com>
2012-04-03 22:05:29 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quoter::Application.config.secret_token = '185bfa363bfa922e42ba9b420f5cf9106540a02af7255a15e836dc8ca9d97de2a4430a03df0c96714c796bdf37509c929765d354f8a977bb4e22e1a39ac1f088'
